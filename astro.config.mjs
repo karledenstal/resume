3f6bed121a18f0833e7e5c3fb55f63d45f6a30af
Karl Edenstål <karl.edenstal@rhyme.se>
2022-11-20 13:55:05 +0100

    mk

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import react from "@astrojs/react";
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), tailwind()],
   output: "server",
   adapter: netlify()
 });
