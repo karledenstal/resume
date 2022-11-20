5d6439629891886b00f0916bd112b7ba541211cf
Karl Edenstål <karl.edenstal@rhyme.se>
2022-11-20 13:53:45 +0100

    try without tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react()],
   output: "server",
   adapter: netlify()
 });
